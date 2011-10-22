8c99bf32cc31c7ae9a01cf1ebc69c79343df2af6
Lassi Tuura <lat@cern.ch>
2011-10-22 09:50:37 +0200

    Add deployment configuration and exercise environment setup.

---
+++ build/env-gcc451.sh
@@ -0,0 +1,9 @@
+export CMS_PATH=$(cd $(dirname $0) && pwd)/current/sw.cms
+export SCRAM_ARCH=slc5_amd64_gcc451 VO_CMS_SW_DIR=$CMS_PATH
+. $VO_CMS_SW_DIR/cmsset_default.sh
+eval $(cd $VO_CMS_SW_DIR/$SCRAM_ARCH/cms/cmssw/CMSSW_4_4_1 && scram runtime -sh)
+
+_gcc_dir=$(dirname $(which gcc))
+function c++-32() { $_gcc_dir/c++ -m32 -Wa,--32 ${1+"$@"}; }
+function g++-32() { $_gcc_dir/g++ -m32 -Wa,--32 ${1+"$@"}; }
+function gcc-32() { $_gcc_dir/gcc -m32 -Wa,--32 ${1+"$@"}; }
---
+++ build/env-gcc461.sh
@@ -0,0 +1,2 @@
+. $(dirname $0)/current/apps/esc/etc/profile.d/init.sh
+export PYTHONHOME=$PYTHON_ROOT # For GDB
