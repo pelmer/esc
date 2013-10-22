c17fc61a39b9b93556ec4c7d70e22b09632983a2
Peter Elmer <elmer@captive-058.wifi.infn.it>
2013-10-22 08:46:31 +0200

    More ESC13

--- exercises/c++/default-ctors-main.cxx
+++
@@ -1,8 +0,0 @@
-#include "default-ctors.h"
-
-int main(int /*argc*/, char **/*argv*/)
-{
-  Z z;
-  z.fct();
-  return 0;
-}
--- exercises/c++/exception-lib1.cxx
+++
@@ -1,6 +0,0 @@
-#include "exception-lib1.h"
-
-double f1(int a) { return 1.0 / a; }
-double f2(int a) { return 2.0 / a; }
-double f3(int a) { return 3.0 / a; }
-
